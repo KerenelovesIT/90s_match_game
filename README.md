1de8b86495971a8fa9211ef70b5f773d3e7398e4
kerenelovesit <thatssewkerene@gmail.com>
2023-07-07 13:31:25 -0400

    memorygame

---
+++ README.md
@@ -0,0 +1,27 @@
+90s Match Game
+
+The 90s Match Game is a basic memory card game where players need to flip cards and find matching pairs. This project uses HTML, CSS, and JavaScript.
+
+How to Play
+
+Launch the game by opening the index.html file in a web browser.
+The game board will display a set of cards faced down.
+Click on a card to flip it and reveal its image.
+Click on a second card to reveal its image.
+If the image and words on both cards match, the cards will remain face up.
+If the images on the cards do not match, both cards will be flipped face down again.
+Continue flipping pairs of cards until all pairs have been matched.
+The game ends when all pairs have been matched.
+
+Features
+Responsive design that works well on both desktop and mobile devices.
+Random shuffling of cards at the start of each game.
+Score tracking to keep count of the number of moves made.
+
+Technologies Used
+HTML
+CSS
+JavaScript
+
+Future updates
+When the game updates it will have different genres to choose from and a range of difficulty from easy to hard.
--- script.js
+++ script.js
@@ -1,130 +1,116 @@
-
-const boadContainer = document.querySelector('.board-container')
+const boardContainer = document.querySelector('.board-container')
 
 let cards = [
-    {name: A1 , img: A1.png }
-    {name: A1 , img: a1.1.png }
-    {name: A2 , img: A2.png}
-    {name: A2 , img: a2.2.png}
-    {name: A3 , img: A3.png }
-    {name: A3 , img: A3.3.png}
-    {name: A4 , img: A4.png}
-    {name: A4 , img: a4.4.png}
-    {name: A5 , img: A5.png}
-    {name: A5 , img: A5.5.png}
-    {name: A6 , img: A6.png}
-    {name: A6 , img: a6.6.png}
-    {name: A7 , img: A7.png}
-    {name: A7 , img: A7.7.png}
-    {name: A8 , img: A8.png}
-    {name: A8 , img: A8.8.png}
-    {name: A9 , img: A9.png}
-    {name: A9 , img: A9.9.png}
-    {name: A10 , img: A10.png}
-    {name: A10 , img: a10.10png}
-    {name: A11 , img: A11.png}
-    {name: A11 , img: a11.11.png}
-    {name: A12 , img: A12.png}
-    {name: A12 , img: a12.12.png}
+    {name: 'A1' , img: 'images/Ginger.png' },
+    {name: 'A1' , img: 'images/Ginger1.png' },
+    {name: 'A2' , img: 'images/A2.png'},
+    {name: 'A2' , img: 'images/a2.2.png'},
+    {name: 'A3' , img: 'images/A3.png' },
+    {name: 'A3' , img: 'images/A3.3.png'},
+    {name: 'A4' , img: 'images/A4.png'},
+    {name: 'A4' , img: 'images/a4.4.png'},
+    {name: 'A5' , img: 'images/A5.png'},
+    {name: 'A5' , img: 'images/A5.5.png'},
+    {name: 'A6' , img: 'images/A6.png'},
+    {name: 'A6' , img: 'images/a6.6.png'},
+    {name: 'A7' , img: 'images/A7.png'},
+    {name: 'A7' , img: 'images/A7.7.png'},
+    {name: 'A8' , img: 'images/A8.png'},
+    {name: 'A8' , img: 'images/A8.8.png'} ,
+    {name: 'A9' , img: 'images/A9.png'} ,
+    {name: 'A9' , img: 'images/A9.9.png'},
+    {name: 'A10' , img: 'images/A10.png'},
+    {name: 'A10' , img: 'images/a10.10.png'},
+    {name: 'A11' , img: 'images/A11.png'},
+    {name: 'A11' , img: 'images/a11.11.png'},
+    {name: 'A12' , img: 'images/A12.png'},
+    {name: 'A12' , img: 'images/a12.12.png'}
 ]
 
 //declare cards
 let firstCard, secondCard
 let lockBoard = false
 let score = 0
-
 document.querySelector('.score').textContent = score
-
+//Must be able to generate cards//
 const generateCards = () => {
-    for (card of cards) {
-        const cardElement = document.createElement('div')
-        cardElement.classList.add('card')
-        cardElement.setAttribute('data-name', card.name)
-        cardElement.innerHTML = `
-        <div class="front">
-            <img class="front-image src="${card.img}" />
-        </div>
-        <div class="back"></div>
-        `
-        boardContainer.appendChild(cardElement)
-        cardElement.addEventListener ('click', flipCard)
-    }
+  for (card of cards) {
+    const cardElement = document.createElement('div')
+    cardElement.classList.add('card')
+    cardElement.setAttribute('data-name', card.name)
+    cardElement.innerHTML = `
+      <div class="front">
+        <img class="front-image" src="${card.img}" />
+      </div>
+      <div class="back"></div>
+    `
+    boardContainer.appendChild(cardElement)
+    cardElement.addEventListener('click', flipCard)
+  }
 }
 generateCards()
-
-
+//get cards to shuffle around to make sure card position unpredictacble//
 const shuffleCards = () => {
-
-    let currentIndex = cards.length,
+  let currentIndex = cards.length,
     randomIndex,
     temporaryValue
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex)
-        currentIndex -=1
-        temporaryValue = cards[currentIndex]
-        cards[currentIndex] = cards[randomIndex]
-        cards[randomIndex] = temporaryValue
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+    temporaryValue = cards[currentIndex]
+    cards[currentIndex] = cards[randomIndex]
+    cards[randomIndex] = temporaryValue
+  }
 }
-
+//Functions get cards to do everything//
+//create flip card function//
+//easier when I seperated cards//
 function flipCard() {
-    if (lockBoard) return
-    if (this === firstCard) return
-
-    this.classList.add('flipped')
-
-    if(!firstCard) {
+  if (lockBoard) return
+  if (this === firstCard) return
+  this.classList.add('flipped')
+  if (!firstCard) {
     firstCard = this
     return
-    }
-
-    secondCard = this 
-    score++
-    document.querySelector('.score').textContent = score
-    lockBoard = true
-    
-    checkForMatch()
+  }
+  secondCard = this
+  score++
+  document.querySelector('.score').textContent = score
+  lockBoard = true
+  checkForMatch()
 }
-
+//create unflipped card function//
 function unflipCards() {
-    ssetTimeout(() => {
-        firstCard.classList.remove('flipped');
-        secondCard.classList.remove('flipped');
-        resetBoard();
-    }, 1000);
-}
-
-
-
-
-function checkForMatch (){
-    let isMatch = firstCard.dataset.name === secondCard.dataset.name
-
-    isMatch ? disableCards() : unflipCards()
-
-} 
-
-
-function disableCards(){
-    firstCard.removeEventListener('click, flipCard')
-    secondCard.removeEventListener('click, flipCard')
-
+  setTimeout(() => {
+    firstCard.classList.remove('flipped')
+    secondCard.classList.remove('flipped')
     resetBoard()
+  }, 1000)
 }
-
+// Check if card match//
+function checkForMatch() {
+  let isMatch = firstCard.dataset.name === secondCard.dataset.name
+  isMatch ? disableCards() : unflipCards()
+}
+// add event listeners//
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard)
+  secondCard.removeEventListener('click', flipCard)
+  resetBoard()
+}
+//get cards to reset and reload//
 function resetBoard() {
-    firstCard = null
-    secondCard = null
-    lockBoard = false
+  firstCard = null
+  secondCard = null
+  lockBoard = false
 }
-
+//have the ability to restart once done//
 function restart() {
-    resetBoard()
-    shuffleCards()
-    score = 0 
-    document.querySelector('.score').textContent = score
-    boardContainer.innerHTML = 
-    generateCards()
+  resetBoard()
+  shuffleCards()
+  score = 0
+  document.querySelector('.score').textContent = score
+  boardContainer.innerHTML = ''
+  generateCards()
 }
-
 document.querySelector('main').addEventListener('click', handleChoice)
