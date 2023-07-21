7dfc5bf27ab00f7e00be6c1b808adf26015d2493
kerenelovesit <thatssewkerene@gmail.com>
2023-07-20 21:33:51 -0400

    fixing image sizes

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 const boardContainer = document.querySelector('.board-container')
 
 let cards = [
-    {name: 'A1' , img: 'images/Ginger.png' },
-    {name: 'A1' , img: 'images/Ginger1.png' },
-    {name: 'A2' , img: 'images/Rugrats.png'},
-    {name: 'A2' , img: 'images/Rugrats1.png'},
-    {name: 'A3' , img: 'images/A3.png' },
-    {name: 'A3' , img: 'images/A3.3.png'},
-    {name: 'A4' , img: 'images/A4.png'},
-    {name: 'A4' , img: 'images/a4.4.png'},
-    {name: 'A5' , img: 'images/A5.png'},
-    {name: 'A5' , img: 'images/A5.5.png'},
-    {name: 'A6' , img: 'images/A6.png'},
-    {name: 'A6' , img: 'images/a6.6.png'},
-    {name: 'A7' , img: 'images/A7.png'},
-    {name: 'A7' , img: 'images/A7.7.png'},
-    {name: 'A8' , img: 'images/A8.png'},
-    {name: 'A8' , img: 'images/A8.8.png'} ,
-    {name: 'A9' , img: 'images/A9.png'} ,
-    {name: 'A9' , img: 'images/A9.9.png'},
-    {name: 'A10' , img: 'images/A10.png'},
-    {name: 'A10' , img: 'images/a10.10.png'},
-    {name: 'A11' , img: 'images/A11.png'},
-    {name: 'A11' , img: 'images/a11.11.png'},
-    {name: 'A12' , img: 'images/A12.png'},
-    {name: 'A12' , img: 'images/a12.12.png'}
+    {id:1, name: 'A1' , img: 'images/Ginger.png' },
+    {id:2, name: 'A1' , img: 'images/Ginger1.png' },
+    {id:3, name: 'A2' , img: 'images/Rugrats.png'},
+    {id:4, name: 'A2' , img: 'images/Rugrats1.png'},
+    {id:5, name: 'A3' , img: 'images/A3.png' },
+    {id:6, name: 'A3' , img: 'images/A3.3.png'},
+    {id:7, name: 'A4' , img: 'images/A4.png'},
+    {id:8, name: 'A4' , img: 'images/a4.4.png'},
+    {id:9, name: 'A5' , img: 'images/A5.png'},
+    {id:10, name: 'A5' , img: 'images/A5.5.png'},
+    {id:11, name: 'A6' , img: 'images/A6.png'},
+    {id:12, name: 'A6' , img: 'images/a6.6.png'},
+    {id:13, name: 'A7' , img: 'images/A7.png'},
+    {id:14, name: 'A7' , img: 'images/A7.7.png'},
+    {id:15, name: 'A8' , img: 'images/A8.png'},
+    {id:16, name: 'A8' , img: 'images/A8.8.png'} ,
+    {id:17, name: 'A9' , img: 'images/A9.png'} ,
+    {id:18, name: 'A9' , img: 'images/A9.9.png'},
+    {id:19, name: 'A10' , img: 'images/A10.png'},
+    {id:20, name: 'A10' , img: 'images/a10.10.png'},
+    {id:21, name: 'A11' , img: 'images/A11.png'},
+    {id:22, name: 'A11' , img: 'images/a11.11.png'},
+    {id:23, name: 'A12' , img: 'images/A12.png'},
+    {id:24, name: 'A12' , img: 'images/a12.12.png'}
 ]
+
 
 //declare cards
 let firstCard, secondCard
@@ -55,7 +56,7 @@ const generateCards = () => {
     cardElement.setAttribute('data-name', card.name)
     cardElement.innerHTML = `
       <div class="front">
-        <img class="front-image" src="${card.img}" />
+        <img class="front-image" src="${card.img}" style="width: 200px; height: 290px;" />
       </div>
       <div class="back"></div>
     `
